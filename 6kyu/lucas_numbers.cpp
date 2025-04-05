576342544b638339a4342a4fa51e86024f811723
gabriel-henri <gabrielhra00@gmail.com>
2025-04-04 22:26:38 -0300

    adicionando instruçoes

--- 6kyu/lucas_numbers.cpp
+++ 6kyu/lucas_numbers.cpp
@@ -1,3 +1,17 @@
+/*
+Lucas numbers are numbers in a sequence defined like this:
+
+L(0) = 2
+L(1) = 1
+L(n) = L(n-1) + L(n-2)
+Your mission is to complete the function that returns the 
+nth term of this sequence.
+
+Note: It should work for negative numbers as well; how you
+ do this is you flip the equation around, so for negative 
+ numbers: L(n) = L(n+2) - L(n+1)
+*/
+
 long long int lucasnum(int n){
     long long int l0 = 2;
     long long int l1 = 1;
